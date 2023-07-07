41d5d8a512201a5efde47a9317b41f5f3eaa52e6
cloudmatt <matthew.morrison@beambenefits.com>
2023-07-07 10:38:35 -0400

    fix renovate

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ module "aws_kms" {
   source = "git@github.com:terraform-aws-modules/terraform-aws-kms.git?ref=v1.0.1"
 }
 
+module "vpc" {
+  # renovate: version=v4.0.2
+  source = "git@github.com:terraform-aws-modules/terraform-aws-vpc.git?ref=faa7ad9cd9a9ec98c4e2dfc1e5b370b4558cd23b"
+}
