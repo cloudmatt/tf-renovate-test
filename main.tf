c08e6596e05bc51a1a5c66e37a777439ced13cb7
cloudmatt <matthew.morrison@beambenefits.com>
2023-07-07 10:25:13 -0400

    chore: add sha example

--- main.tf
+++ main.tf
@@ -9,3 +9,16 @@ module "aws_kms" {
   enable_key_rotation     = true
   policy                  = data.aws_iam_policy_document.iam_policy.json
 }
+
+module "aws_kms" {
+  # renovate: version=v1.0.1
+  source = "git@github.com:terraform-aws-modules/terraform-aws-kms.git?ref=693ec0295f4faa1430d7b039ba30ad89363a7b5c"
+
+  is_enabled              = true
+  aliases                 = ["${data.aws_iam_account_alias.current.account_alias}-${var.name}-key"]
+  description             = "Backup KMS encryption key for ${var.name}"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.iam_policy.json
+}
+
